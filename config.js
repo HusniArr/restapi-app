1906d19a9709a074379b1ec93fe2bc4a929b7c0d
husni <husni@siklus.id>
2021-06-07 16:48:16 +0700

    create tabel user

--- config.js
+++ config.js
@@ -3,9 +3,8 @@ const mysql = require('mysql');
 const conn = mysql.createConnection({
 	host:"localhost",
 	user:"root",
-	password:""
+	password:"",
+	database:"db_restapi"
 })
-
-
 
 module.exports = conn;
--- server.js
+++ server.js
@@ -11,13 +11,12 @@ app.get('/',(req,res)=>{
 	res.send('hello world');
 })
 
-
 conn.connect((err)=>{
 	if(err) throw err;
 	console.log('db mysql berhasil terkoneksi.');
-	conn.query("CREATE DATABASE db_restapi",(err,res)=>{
+	conn.query("CREATE TABLE users (id_user int auto_increment primary key, username varchar(20) not null , pass varchar(8) not null, level varchar(20) not null)",(err,res)=>{
 		if(err) throw err;
-		console.log('Database berhasil dibuat.')
+		console.log('tabel berhasil dibuat.')
 	})
 })
 
