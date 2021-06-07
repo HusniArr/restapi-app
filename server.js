8952f7bd01584840d3454a7074cbadd917040c43
husni <husni@siklus.id>
2021-06-07 16:31:09 +0700

    create database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const conn = require('./config');
 const app = express();
 const port = process.env.PORT || 2021;
 
@@ -8,6 +9,16 @@ app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
 	res.send('hello world');
+})
+
+
+conn.connect((err)=>{
+	if(err) throw err;
+	console.log('db mysql berhasil terkoneksi.');
+	conn.query("CREATE DATABASE db_restapi",(err,res)=>{
+		if(err) throw err;
+		console.log('Database berhasil dibuat.')
+	})
 })
 
 app.listen(port,(req,res)=>{
